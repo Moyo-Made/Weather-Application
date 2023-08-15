afc052a82480cb2cfd52a905c9b80de1aab21625
Moyomade <adegbitemoyomade2004@gmail.com>
2023-08-15 23:56:08 +0100

    Update app.js

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,6 +1,7 @@
-import apiKey from "./apikey.js"
+
 
 let weather = {
+    apiKey: "ff4fc8d7f3a1751b99c5118599ce6e7a";
     fetchWeather: function(city){
         fetch("https://api.openweathermap.org/data/2.5/weather?q="
         + city 
